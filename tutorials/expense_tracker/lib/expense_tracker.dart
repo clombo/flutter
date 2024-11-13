6b48c210bdb6a3f3ca240f181be1982dec4a9ff4
clombo <christo.lombard@live.co.za>
2024-11-13 19:32:36 +0200

    Started with expense tracker

---
+++ tutorials/expense_tracker/lib/expense_tracker.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class ExpenseTracker extends StatefulWidget{
+  const ExpenseTracker({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ExpenseTracker();
+}
+
+class _ExpenseTracker extends State<ExpenseTracker>{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
